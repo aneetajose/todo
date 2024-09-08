1f6a9cab52c97e1df13004ae901950a9d922ce93
Aneeta Jose <itsaneetajose@gmail.com>
2024-09-08 20:18:55 +0530

    Styling the form

--- todoapp/src/App.jsx
+++ todoapp/src/App.jsx
@@ -1,8 +1,11 @@
+import Header from "./components/Header";
 import Todo from "./components/Todo";
+import "./App.css";
 
 function App() {
   return (
     <>
+      <Header />
       <Todo />
     </>
   );
